d6105f8b6f85998f4dc2e808be89c6bb404f727e
Mathiio <matheo.pougalan@gmail.com>
2024-05-21 16:01:29 +0200

    add: icons

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -14,7 +14,7 @@ export const Home = () => {
       <div className='w-full flex flex-row gap-75'>
         <section className='w-[60%] flex flex-col gap-50'>
           <div className='w-full flex flex-col gap-25'>
-            <NavKeyWords numberOfButtons={50} />
+            <NavKeyWords numberOfButtons={16} />
             <YouTubeVideo videoUrl='56STvMBKYdw?si=6TJjovmA3ezE4_3v' />
           </div>
           <div className='w-full flex flex-col gap-25'>
