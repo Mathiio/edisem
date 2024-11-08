aabb79a94fe1e1b5f67b79aaa990720a7d0a0eea
erwan-afk <erwan.thibaud@edu.univ-eiffel.fr>
2024-11-07 22:10:00 -0500

    Page dataviz

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Database } from '@/pages/database';
 import { Edition } from '@/pages/edition';
 import LoginPage from '@/pages/login';
 import { withAuth } from '@/pages/withAuth';
+import Datavisualisation from './pages/datavisualisation';
 
 const ProtectedDatabase = withAuth(Database);
 
@@ -21,6 +22,7 @@ function App() {
       <Route index path='/' Component={Home} />
       <Route path='/edition/:id/:title?' Component={Edition} />
       <Route path='/login' Component={LoginPage} />
+      <Route path='/datavisualisation' Component={Datavisualisation} />
     </Routes>
   );
 }
