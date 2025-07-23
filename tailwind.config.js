5382701298d3c8d4e4fa376bc44b8a577d6cd4e1
Mathiio <matheo.pougalan@gmail.com>
2025-07-23 17:19:26 +0200

    fix: chart bars

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,44 +13,4 @@ module.exports = {
   },
   darkMode: 'class',
   plugins: [heroui()],
-};
-
-// module.exports = {
-//   content: [
-//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-//     './src/**/*.{js,ts,jsx,tsx,mdx}',
-//     './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
-//   ],
-//   presets: [require('./src/theme/tailwind.preset')],
-//   theme: {
-//     extend: {},
-//   },
-//   darkMode: 'class',
-//   plugins: [
-//     function ({ addUtilities }) {
-//       const newUtilities = {
-//         ".scrollbar-thin": {
-//           scrollbarWidth: "thin",
-//           scrollbarColor: "rgb(31 29 29) white"
-//         },
-//         ".scrollbar-webkit": {
-//           "&::-webkit-scrollbar": {
-//             width: "2vw"
-//           },
-//           "&::-webkit-scrollbar-track": {
-//             background: "white"
-//           },
-//           "&::-webkit-scrollbar-track": {
-//             backgroundColor: "rgb(31 41 55)",
-//             borderRadius: "20px",
-//             border: "1px solid white",
-//           },
-//           "::-webkit-scrollbar-button:vertical:start:increment, ::-webkit-scrollbar-button:vertical:end:decrement, ::-webkit-scrollbar-button:horizontal:end:increment, ::-webkit-scrollbar-button:horizontal:end:decrement": {
-//             display: "none"
-//           }
-//         }
-//       }
-//       addUtilities(newUtilities, ["responsive", "hover"])
-//     }
-//   ],
-// };
+}
